059854f72424d48cb2a5774f5729676c9347ca8f
Ceye4n <129878378+Ceye4n@users.noreply.github.com>
2023-05-26 19:54:33 +0200

    clear

--- .fleet/chatgpt.py
+++
@@ -1,4 +0,0 @@
-import openai
-openai.api_key = "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"
-print(openai.Model.list())    
-completion = openai.Completion.create(model="ada", prompt="Hello world")
